738c4227a0b99d9d3c3832d461be29c54934e20c
Fernando Schwalb <fschwalb@gmail.com>
2021-11-25 02:18:21 -0300

    MultipleCustomHooks Modified 2

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
-import '../02-useEffect/effects.css'
+import { useCounter } from '../../hooks/useCounter';
+import { useFetch } from '../../hooks/useFetch';
+
+import '../02-useEffect/effects.css';
 
 export const MultipleCustomHooks = () => {
+
+    const { counter, increment } = useCounter(1);
+
+    const { loading, data } = useFetch( `https://www.breakingbadapi.com/api/quotes/${ counter }` );
+
+    const { author, quote } = !!data && data[0]
+
     return (
         <div>
-            <h1>Custom Hooks!</h1>
+            <h1>Breaking Bad Quotes</h1>
             <hr />
-            
+
+            {
+                loading
+                ?
+                    (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    )
+                :
+                    (
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-3">{ quote }</p>
+                            <footer className="blockquote-footer">{ author }</footer>
+                        </blockquote>
+                    )
+            }
+
+            <button 
+                className="btn btn-outline-primary" 
+                onClick={ increment }
+            >
+                Next Quote
+            </button>
         </div>
     );
+
 };
