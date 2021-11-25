28778d88a3bf0faecb98a096628380e6a8f11933
Fernando Schwalb <fschwalb@gmail.com>
2021-11-25 01:22:49 -0300

    MultipleCustomHooks Added

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { HooksApp } from './HooksApp';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 
 ReactDOM.render(
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 );
