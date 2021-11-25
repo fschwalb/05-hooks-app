2e1d2d3caec6d399325bf4c262f5502e2f0885d6
Fernando Schwalb <fschwalb@gmail.com>
2021-11-25 01:32:45 -0300

    MultipleCustomHooks Modified 1

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -5,7 +5,7 @@ import '../02-useEffect/effects.css'
 export const MultipleCustomHooks = () => {
     return (
         <div>
-            <h1>Custom Hooks!!</h1>
+            <h1>Custom Hooks!</h1>
             <hr />
             
         </div>
