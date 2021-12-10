d99128f6ad9b9a6875919e182538562803df990f
Fernando Schwalb <fschwalb@gmail.com>
2021-12-10 02:24:30 -0300

    UserContext Added

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
+
 
 export const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Fernando Schwalb',
+        email: 'fernando.schwalb@gmail.com'
+    }
+
     return (
-        <div>
+        <UserContext.Provider value={ user }>
+            
             <AppRouter />
-        </div>
+
+        </UserContext.Provider>
     );
+
 };
