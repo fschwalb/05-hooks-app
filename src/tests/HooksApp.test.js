919d1adcb80d2ddc0ffb493f9af461f229ba7b78
Fernando Schwalb <fschwalb@gmail.com>
2022-03-01 17:34:45 -0300

    HooksApp.test.js Added

---
+++ src/tests/HooksApp.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { HooksApp } from '../HooksApp';
+
+
+describe('Pruebas en <HookApp />', () => { 
+
+    test('Debe mostrarse correctamente', () => { 
+
+        const wrapper = shallow( <HooksApp /> );
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+
+});
