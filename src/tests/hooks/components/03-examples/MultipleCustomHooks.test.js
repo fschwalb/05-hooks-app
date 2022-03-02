00deb64a8ca635422c453b0664c5fe1179534f65
Fernando Schwalb <fschwalb@gmail.com>
2022-03-02 12:53:03 -0300

    MultipleCustomHook.test.js Added OK

---
+++ src/tests/hooks/components/03-examples/MultipleCustomHooks.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import { MultipleCustomHooks } from '../../../../components/03-examples/MultipleCustomHooks';
+import { useFetch } from '../../../../hooks/useFetch';
+jest.mock('../../../../hooks/useFetch');
+
+describe('Pruebas en <MultipleCustomHooks />', () => { 
+   
+    test('Debe de mostrarse correctamente', () => { 
+
+        useFetch.mockReturnValue({
+            data: null,
+            loading: true,
+            error: null
+        });
+
+        const wrapper = shallow( <MultipleCustomHooks /> );
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('Debe de mostrar la información', () => { 
+
+        useFetch.mockReturnValue({
+            data: [{
+                author: 'Fernando',
+                quote: 'Hola Mundo'
+            }],
+            loading: false,
+            error: null
+        });
+
+        const wrapper = shallow( <MultipleCustomHooks /> );
+
+        expect( wrapper.find('.alert').exists() ).toBe( false );
+        expect( wrapper.find('.mb-3').text().trim() ).toBe( 'Hola Mundo' );
+        expect( wrapper.find('footer').text().trim() ).toBe( 'Fernando' );
+
+    });
+    
+});
